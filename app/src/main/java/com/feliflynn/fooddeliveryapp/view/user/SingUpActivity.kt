a6f8be133563deb6973bcc8be5e954ba09bf9b40
felipeflynn <feliflynn@gmail.com>
2022-01-29 21:21:00 -0300

    login

--- app/src/main/java/com/feliflynn/fooddeliveryapp/view/user/SingUpActivity.kt
+++ app/src/main/java/com/feliflynn/fooddeliveryapp/view/user/SingUpActivity.kt
@@ -2,6 +2,7 @@ package com.feliflynn.fooddeliveryapp.view.user
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.feliflynn.fooddeliveryapp.R
 
 class SingUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
